59bffe109135bad562639245fa34286eaa5c9ba3
vk467 <vkvihari467@gmail.com>
2023-01-08 13:24:10 +0530

    JSX Basics

--- App.js
+++ App.js
@@ -1,30 +1,50 @@
-const heading = React.createElement(
-    "h1",
-    {
-      id: "title",
-    },
-    "Heading 1"
+/****
+ * 
+ * Parcel - features
+ * 
+*HMR Hot Module Reloading
+* File Watcher algorithm C++
+* BUNDLING
+* MINIFY
+*Cleaning our Code
+*Dev and Production Build
+*Super Fast build algorithm
+*Image optimization
+*Caching while development
+*Compression
+*Compatble with older version of browser
+*HTTPS on dev
+*port Number
+*Consistent Hashing Algorithm
+*Zero Config
+* Tree Shaking = remove unwanted
+ * 
+ * 
+ */
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const title = (
+  <h1>React App</h1>
+);
+
+//JSX -> React.CreateElement -> Object -> HTML(DOM)
+//Babel converts JSX to React element
+//component composition = using one component in another component
+
+const Component = () =>{
+  return(
+    <div>
+      {title}
+      <h2>React functional component</h2>
+    </div>
   );
-  
-  const heading2 = React.createElement(
-    "h2",
-    {
-      id: "title",
-    },
-    "Heading 2"
-  );
-  
-  const container = React.createElement(
-    "div",
-    {
-      id: "container",
-    },
-    [heading, heading2]
-  );
-  
-  console.log(heading);
+}
+ 
   
   const root = ReactDOM.createRoot(document.getElementById("root"));
   
-  root.render(container);
+  root.render(<Component />);
   
